5b8d359c65539601c38c8ffd7db12861f0137f85
gosongs <go_songs@163.com>
2017-11-03 16:45:58 +0800

    dashboard

--- src/app/pages/machine/list/list.component.ts
+++ src/app/pages/machine/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,138 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  validateForm: FormGroup;
+  controlArray = [];
+  isCollapse = true;
+  data = [
+    {
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+    },
+    {
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+    },
+    {
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+    },
+    {
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+    },
+    {
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+    }
+  ];
+
+  toggleCollapse() {
+    this.isCollapse = !this.isCollapse;
+    this.controlArray.forEach((c, index) => {
+      c.show = this.isCollapse ? (index < 6) : true;
+    });
+  }
+
+  resetForm() {
+    this.validateForm.reset();
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.validateForm = this.fb.group({});
+
+    for (let i = 0; i < 10; i++) {
+      this.controlArray.push({ index: i, show: i < 6 });
+      this.validateForm.addControl(`field${i}`, new FormControl());
+    }
   }
 
 }
--- src/app/pages/machine/machine.module.ts
+++ src/app/pages/machine/machine.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { MachineRoutingModule } from './machine-routing.module';
@@ -7,6 +9,9 @@ import { MachineRoutingModule } from './machine-routing.module';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgZorroAntdModule,
     MachineRoutingModule
   ],
   declarations: [ListComponent, DetailComponent]
